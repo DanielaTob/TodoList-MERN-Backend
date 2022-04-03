f4dc7cf67d153ef14487e3d76129ba549a4b7c6e
daniela <moreno.tobar.daniela@gmail.com>
2022-04-03 15:56:17 -0500

    config local back

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors')
 
 const { mongoose } = require('./database');
 
@@ -14,6 +15,7 @@ app.set('port', process.env.PORT || 4000);
 
 app. use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 
 // Routes
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,36 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors')
+
+var corsOptions = {
+    origin: 'http://localhost:3001',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
 
 const Task = require('../models/tasks');
 
-router.get('/', async (req, res) =>{
+router.get('/',cors(corsOptions), async (req, res) =>{
     const tasks = await Task.find()
     res.json(tasks);
 });
 
 //Only Task
 
-router.get('/:id', async (req, res) =>{
+router.get('/:id',cors(corsOptions), async (req, res) =>{
     const task = await Task.findById(req.params.id);
     res.json(task);
 })
 
 //Task Saved
-router.post('/', async (req, res) =>{
+router.post('/', cors(corsOptions), async (req, res) =>{
     const { title, description } = req.body;
     const task = new Task({
         title: title,
         description: description
     })
-    await task.save();
+    await task.save(corsOptions);
     res.json({status: 'Task Saved'});
 });
 
 
 //Task Update
 
-router.put('/:id', async (req, res) =>{
+router.put('/:id',cors(corsOptions), async (req, res) =>{
     const { title, description } = req.body;
     const newTask = Object.assign({ title, description });
     await Task.findByIdAndUpdate(req.params.id, newTask);
@@ -40,7 +46,7 @@ router.put('/:id', async (req, res) =>{
 
 //Task Delete 
 
-router.delete('/:id', async (req, res) =>{
+router.delete('/:id',cors(corsOptions), async (req, res) =>{
     await Task.findByIdAndRemove(req.params.id);
     res.json({status: 'Task Deleted'});
 });
