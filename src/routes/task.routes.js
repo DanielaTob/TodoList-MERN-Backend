84a71ecf35e0dc3500ad04c0c941227765b5b12a
daniela <moreno.tobar.daniela@gmail.com>
2022-04-04 12:18:45 -0500

    form done with method post

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cors = require('cors')
 
 var corsOptions = {
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
